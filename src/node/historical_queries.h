eb86e057baba5239c59836652e07ca0189e7c1a0
Eddy Ashton <edashton@microsoft.com>
2020-06-10 11:33:25 +0100

    Bah, copy trees!

--- src/node/historical_queries.h
+++ src/node/historical_queries.h
@@ -195,7 +195,8 @@ namespace ccf::historical
           if (request.history == nullptr)
           {
             LOG_INFO_FMT("Yes!");
-            request.history = sig_tree;
+            request.history =
+              std::make_shared<ccf::MerkleTreeHistory>(sig_tree->serialise());
             request.next_history_entry = sig_idx + 1;
             fetch_entry_at(request.next_history_entry);
           }
@@ -290,7 +291,6 @@ namespace ccf::historical
             {
               // We were looking for this entry. Store the produced store
               request.current_stage = RequestStage::Untrusted;
-              // request.entry_hash = crypto::Sha256Hash(entry);
               request.store = store;
             }
             else
@@ -304,7 +304,6 @@ namespace ccf::historical
 
           // Add hash of this entry to any Untrusted entries which are looking
           // for it
-          crypto::Sha256Hash entry_hash(entry);
           for (auto it = requests.begin(); it != requests.end(); ++it)
           {
             auto& request = it->second;
@@ -312,7 +311,17 @@ namespace ccf::historical
             {
               LOG_INFO_FMT(
                 "Appending hash of {} to history for {}", idx, it->first);
+              crypto::Sha256Hash entry_hash(entry);
+              uint8_t* data = entry_hash.h.data();
+              constexpr auto size = crypto::Sha256Hash::SIZE * 2;
+              using T = std::vector<uint8_t>;
+              T before(data, data + size);
+              LOG_INFO_FMT(
+                "Before calling append: {:02x}", fmt::join(before, " "));
               request.history->append(entry_hash);
+              T after(data, data + size);
+              LOG_INFO_FMT(
+                "After calling append: {:02x}", fmt::join(after, " "));
               request.next_history_entry += 1;
               fetch_entry_at(request.next_history_entry);
             }
