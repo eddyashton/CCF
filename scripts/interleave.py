432538eefdc40e9df3391e053a132ad4278fd1f8
Eddy Ashton <edashton@microsoft.com>
2021-06-16 09:59:15 +0100

    Add regex rewriter

--- scripts/interleave.py
+++ scripts/interleave.py
@@ -1,8 +1,21 @@
 import argparse
+import functools
 import re
+from colour import Color
 from contextlib import ExitStack
 from datetime import datetime
-from colour import Color, RGB_color_picker
+
+args = None
+
+
+def compose(*fs):
+    def compose2(f, g):
+        def fn(*args, **kwargs):
+            return f(g(*args, **kwargs))
+
+        return fn
+
+    return functools.reduce(compose2, fs)
 
 
 def make_rescaler(from_min, from_max, to_min, to_max):
@@ -28,12 +41,11 @@ def colour_highlight(*args, **kwargs):
     red, green, blue = (int(n * 255) for n in c.rgb)
 
     def fn(s):
+        if s is None:
+            return None
         return f"\033[48;2;{red};{green};{blue}m{s}\033[0m"
 
     return fn
-
-
-assigned_decorators = {}
 
 
 def find_id_in_log(obj):
@@ -52,10 +64,46 @@ def find_id_in_log(obj):
         pass
 
 
+assigned_indents = {}
+assigned_indices = {}
+
+
+def regex_rewriter(key, regex_s, format_string):
+    regex = re.compile(regex_s)
+    if not key in assigned_indices:
+        assigned_indices[key] = len(assigned_indices)
+    if not key in assigned_indents:
+        assigned_indents[key] = "  " * len(assigned_indents)
+
+    def fn(s):
+        m = regex.match(s)
+        if not m:
+            return None
+        format_dict = {
+            "indent": assigned_indents[key],
+            "index": assigned_indices[key],
+            "original": s,
+        }
+        for group_name in regex.groupindex.keys():
+            format_dict[group_name] = m.group(group_name) or ""
+        return format_string.format(**format_dict)
+
+    return fn
+
+
+assigned_decorators = {}
+
+
 def decorated_line(line, key):
     if key not in assigned_decorators:
-        log_id = find_id_in_log(key)
-        assigned_decorators[key] = colour_highlight(pick_for=log_id or key, pick_key=str)
+        log_id = find_id_in_log(key) or key
+        decorator_fns = []
+        if args.colour:
+            decorator_fns.append(colour_highlight(pick_for=log_id, pick_key=str))
+        decorator_fns.append(
+            regex_rewriter(key, args.line_parsing_regex, args.output_format)
+        )
+        assigned_decorators[key] = compose(*decorator_fns)
     return assigned_decorators[key](line)
 
 
@@ -76,7 +124,9 @@ def next_timestamped_block_in_file(fp):
             prev_time = next_time
         except ValueError:
             pass
-        lines.append(decorated_line(nextline.rstrip(), fp))
+        decorated = decorated_line(nextline.rstrip(), fp)
+        if decorated is not None:
+            lines.append(decorated)
         nextline = fp.readline()
     yield prev_time, lines
 
@@ -97,7 +147,7 @@ def next_block_from_files(fps):
             pass
 
 
-def print_interleaved_files(args):
+def print_interleaved_files():
     with ExitStack() as stack:
         files = [stack.enter_context(open(file)) for file in args.files]
 
@@ -106,13 +156,33 @@ def print_interleaved_files(args):
                 print(line)
 
 
+CCF_LOG_LINE_REGEX = r"(?P<prefix>(?P<datetime>.*Z)\s+(?P<timeoffset>\S+)?\s+(?P<thread_id>\d+)\s+\[(?P<level>\w+)\s?\]\s+(?P<filename>.*):(?P<linenumber>\d+)\s+\| )?(?P<content>.*$)"
+REWRITTEN_LOG_LINE = "{datetime:27} |{index:02}| {indent}{content}"
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description="Interleave the output of several log files according to their timestamps, colouring each uniquely"
     )
 
     parser.add_argument("files", nargs="+", help="List of files to interleave")
+    parser.add_argument(
+        "--colour",
+        action="store_true",
+        help="Add unique background colours for each input log",
+        default=False,
+    )
+    parser.add_argument(
+        "--line-parsing-regex",
+        help="Regex used to parse incoming log lines, producing groups which can be substituted into output lines in --output-format. Default parses expected lines from CCF node logs",
+        default=CCF_LOG_LINE_REGEX,
+    )
+    parser.add_argument(
+        "--output-format",
+        help="Format string used to rewrite each input line",
+        default=REWRITTEN_LOG_LINE,
+    )
 
+    # Deliberately global, visible to functions above. Python!
     args = parser.parse_args()
 
-    print_interleaved_files(args)
+    print_interleaved_files()
