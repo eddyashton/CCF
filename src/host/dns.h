ac3d794f5d035b66e139fc2c81ff0d299c008e54
Eddy Ashton <edashton@microsoft.com>
2022-02-01 14:29:52 +0000

    Temp: Add a delay, to trigger the crash

--- src/host/dns.h
+++ src/host/dns.h
@@ -11,12 +11,26 @@ namespace asynchost
   class DNS
   {
   public:
-    static bool resolve(
-      const std::string& host,
-      const std::string& service,
-      void* ud,
-      uv_getaddrinfo_cb cb,
-      bool async)
+    static void sleep_rand(uv_work_t* req)
+    {
+      const auto sleep_time = 100 + (rand() % 500);
+      LOG_INFO_FMT(
+        "Pausing {}ms before resolving, on thread {}",
+        sleep_time,
+        std::this_thread::get_id());
+      std::this_thread::sleep_for(std::chrono::milliseconds(sleep_time));
+    }
+
+    struct AsyncResolveArgs
+    {
+      std::string host;
+      std::string service;
+      void* ud;
+      uv_getaddrinfo_cb cb;
+      uv_getaddrinfo_t* resolver;
+    };
+
+    static void async_resolve(uv_work_t* req, int status)
     {
       struct addrinfo hints;
       hints.ai_family = AF_UNSPEC;
@@ -24,34 +38,66 @@ namespace asynchost
       hints.ai_protocol = IPPROTO_TCP;
       hints.ai_flags = 0;
 
+      AsyncResolveArgs* args = (AsyncResolveArgs*)req->data;
+
+      int rc;
+      if (
+        (rc = uv_getaddrinfo(
+           uv_default_loop(),
+           args->resolver,
+           args->cb,
+           args->host.c_str(),
+           args->service.c_str(),
+           &hints)) < 0)
+      {
+        LOG_FAIL_FMT(
+          "uv_getaddrinfo for host:service [{}:{}] failed (async) with error "
+          "{}",
+          args->host,
+          args->service,
+          uv_strerror(rc));
+        delete args->resolver;
+      }
+
+      delete args;
+      delete req;
+    }
+
+    static bool resolve(
+      const std::string& host,
+      const std::string& service,
+      void* ud,
+      uv_getaddrinfo_cb cb,
+      bool async)
+    {
       auto resolver = new uv_getaddrinfo_t;
       resolver->data = ud;
 
-      int rc;
-
       if (async)
       {
-        if (
-          (rc = uv_getaddrinfo(
-             uv_default_loop(),
-             resolver,
-             cb,
-             host.c_str(),
-             service.c_str(),
-             &hints)) < 0)
+        uv_work_t* work_req = new uv_work_t;
         {
-          LOG_FAIL_FMT(
-            "uv_getaddrinfo for host:service [{}:{}] failed (async) with error "
-            "{}",
-            host,
-            service,
-            uv_strerror(rc));
-          delete resolver;
-          return false;
+          auto req_data = new AsyncResolveArgs;
+          req_data->host = host;
+          req_data->service = service;
+          req_data->ud = ud;
+          req_data->cb = cb;
+          req_data->resolver = resolver;
+          work_req->data = req_data;
         }
+        uv_queue_work(uv_default_loop(), work_req, sleep_rand, async_resolve);
+        return true;
       }
       else
       {
+        struct addrinfo hints;
+        hints.ai_family = AF_UNSPEC;
+        hints.ai_socktype = SOCK_STREAM;
+        hints.ai_protocol = IPPROTO_TCP;
+        hints.ai_flags = 0;
+
+        int rc;
+
         if (
           (rc = uv_getaddrinfo(
              uv_default_loop(),
