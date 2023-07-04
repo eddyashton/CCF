14d35ba3695475694e54c16865034b4cf175df68
Eddy Ashton <edashton@microsoft.com>
2023-07-04 16:27:32 +0000

    Minimal lock stack sanity check

--- src/ds/named_mutex.h
+++ src/ds/named_mutex.h
@@ -5,31 +5,63 @@
 #include "ccf/ds/logger.h"
 #include "ccf/pal/locking.h"
 
+#include <stack>
+
+#define ASSERT_MUTEX_ORDER
+
 namespace ds
 {
+#ifdef ASSERT_MUTEX_ORDER
+  static thread_local std::stack<std::string> locked_stack;
+#endif
+
   class NamedMutex
   {
   private:
     ccf::pal::Mutex m;
     std::string label;
 
+#ifdef ASSERT_MUTEX_ORDER
+    void check_lock_safety()
+    {
+      locked_stack.push(label);
+    }
+#endif
+
   public:
     NamedMutex(const std::string& s) : label(s) {}
 
     void lock()
     {
-      LOG_INFO_FMT("Locking {}", label);
+#ifdef ASSERT_MUTEX_ORDER
+      check_lock_safety();
+#endif
+
       m.lock();
     }
 
     bool try_lock()
     {
+#ifdef ASSERT_MUTEX_ORDER
+      check_lock_safety();
+#endif
+
       return m.try_lock();
     }
 
     void unlock()
     {
-      LOG_INFO_FMT("Unlocking {}", label);
+#ifdef ASSERT_MUTEX_ORDER
+      if (locked_stack.top() != label)
+      {
+        LOG_FAIL_FMT("Unexpected unlock order - {} is not the last-locked", label);
+      }
+      else
+      {
+        locked_stack.pop();
+      }
+#endif
+
       m.unlock();
     }
   };
